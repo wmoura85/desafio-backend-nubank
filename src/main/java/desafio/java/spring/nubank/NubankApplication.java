0092c35e2770467961d0a273141cd1f3f05ca274
William <wmoura85@gmail.com>
2025-04-17 00:56:30 -0300

    :tada: Primeiro commit com a estrutura do desafio

---
+++ src/main/java/desafio/java/spring/nubank/NubankApplication.java
@@ -0,0 +1,13 @@
+package desafio.java.spring.nubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/java/spring/nubank/NubankApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.java.spring.nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
